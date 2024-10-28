3d1e96d3cf353835ce5e2d9ce7ae631e1526774c
holly <h.person.notrobot@gmail.com>
2024-10-28 15:42:17 +0000

    Added csv output, and compilation to DataFrame

--- src/main.py
+++ src/main.py
@@ -1,14 +1,26 @@
 from data_loading import load_innings
 from bounce import calc_bounce
+import pandas as pd
 
 def main():
+    data = { # Dataframe format for output.
+        "BounceX": [],
+        "BounceY": [],
+        "BounceFloat": []
+    }
+    data = pd.DataFrame(data) # Convert "data" into a pandas DataFrame
+
+
     # I have a vague recollection that windows requires \ rather than / for file paths?
     inning = load_innings("./data/data.csv")
     for index, row in inning.iterrows():
         # Whatever processing happens should be decided by Understanding the data 1&2.
         # This is a placeholder. Position should probably also be tracked. Later problem.
         bounce_value = calc_bounce(row)
-        print(bounce_value)
+        data.loc[len(data.index)] = [row["BounceX"], row["BounceY"], bounce_value] # Add each calculated bounce value to the DataFrame
+    data.to_csv("out.csv", index=False)
+    print(data)
 
 if __name__ == "__main__":
     main()
+
