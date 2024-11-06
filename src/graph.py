20dd974acb46ab90fc9d1c995e2c93b08f5f0e03
holly <h.person.notrobot@gmail.com>
2024-11-06 16:47:13 +0000

    Added pitch size plot and wicket drawing

--- src/graph.py
+++ src/graph.py
@@ -2,11 +2,22 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 def plot(innings): # Bounces using the dataframe from main()
     # 1st plot: Scatter plot for bounce positions
     
+    # Fix the plot to the pitch size and dimentions
     plt.figure(figsize=(10, 6))
+    plt.xlim(-11.28, 11.28)
+    plt.ylim(-1.82, 1.83)
+
+    # Draw the wickets
+    currentAxis = plt.gca()
+    currentAxis.add_patch(Rectangle((10.06, -0.2), 0.125, 0.4, facecolor="none", ec='k', lw=2))
+    currentAxis.add_patch(Rectangle((-10.06, -0.2), -0.125, 0.4, facecolor="none", ec='k', lw=2))
+    
+    # Draw all the bounce datapoints
     plt.scatter(innings["BounceX"], innings["BounceY"], c="green",alpha=innings["BounceFloat"],label='Bounce Position')
     plt.title("Ball Bounce Position")
     plt.xlabel("X Position (m)")
